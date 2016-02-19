45b9f5be293f8cbbe1649033d610d7f7e64188fe
olafmoriarty <olafmoriarty@gmail.com>
2016-02-19 05:33:31 -0500

    User login functionality added

--- _ns/plugins/users/config.php
+++ _ns/plugins/users/config.php
@@ -3,6 +3,8 @@
 	$tpf = NS_PATH.'plugins/'.basename(dirname(__FILE__)).'/';
 
 	// Setup 
+	$n_urls->add_line(['url' => 'login', 'script' => $tpf.'login.php']);
 	$n_urls->add_line(['url' => 'register', 'script' => $tpf.'register.php']);
+	$n_urls->add_line(['url' => 'welcome', 'script' => $tpf.'welcome.php']);
+
 	$n_menu->add_line(['text' => __('New user?'), 'link' => '/n/register/']);
-?>
--- _ns/plugins/users/register.php
+++ _ns/plugins/users/register.php
@@ -7,21 +7,55 @@
     $submitted = true;
 
     // VALIDATE DATA AND ADD TO DATABASE
+    $validate_input_table = 'ns_users';
+    if (validate_input(['check' => 'unique', 'input' => 'reg_username', 'field' => 'username', error => __('Sorry! The chosen username is already in use.')])) {
+      $errors = true;
+    }
+    
+    if (!$errors) {
+      $table = 'ns_users';
+      $fields = array();
+      $values = array();
+      
+      // Username
+      
+      $fields[] = 'username';
+      $values[] = $_POST['reg_username'];
+      
+      // E-mail
+      
+      $fields[] = 'email';
+      $values[] = $_POST['reg_email'];
+      
+      // Password and salt
+      $random_salt = hash('sha512', uniqid(openssl_random_pseudo_bytes(16), TRUE));
+
+      $fields[] = 'password';
+      $values[] = hash('sha512', $_POST['reg_pass'] . $random_salt);
+
+      $fields[] = 'salt';
+      $values[] = $random_salt;
+      
+      $query = 'INSERT INTO '.$table.' ('.implode(', ', $fields).') VALUES (\''.implode('\', \'', $values).'\')';
+      $conn->query($query);
+
+      $c .= 'Success';
+    
+    }
   }
 
   if (!$submitted || $errors) {
     // Registration form
     $c .= '<h2>'.__('Register new user').'</h2>'."\n";
-    $c .= '<form method="post" action="/n/register/">'."\n";
+    $c .= '<form method="post" name="registration_form" action="/n/register/">'."\n";
     $c .= '<p>'.__('Your preferred username:').'<br>'."\n";
-    $c .= '<input type="text" name="reg_username"></p>'."\n";
+    $c .= '<input type="text" name="reg_username" id="reg_username"></p>'."\n";
     $c .= '<p>'.__('Your e-mail:').'<br>'."\n";
-    $c .= '<input type="text" name="reg_email"></p>'."\n";
+    $c .= '<input type="text" name="reg_email" id="reg_email"></p>'."\n";
     $c .= '<p>'.__('Your password:').'<br>'."\n";
-    $c .= '<input type="password" name="reg_pass"></p>'."\n";
+    $c .= '<input type="password" name="reg_pass" id="reg_pass"></p>'."\n";
     $c .= '<p>'.__('Repeat password:').'<br>'."\n";
-    $c .= '<input type="password" name="reg_pass2"></p>'."\n";
+    $c .= '<input type="password" name="reg_pass2" id="reg_pass2"></p>'."\n";
+    $c .= '<p><input type="submit" name="reg_button" value="'.__('Register!').'"></p>';
     $c .= '</form>'."\n";
   }
-  
-?>
