3274d64361f7d1b2ae936a9655642faa64ac426d
Olaf Moriarty Solstrand <olafmoriarty@gmail.com>
2016-02-20 17:52:29 +0100

    E-mail verification added (the "sending e-mail" part)

--- _ns/plugins/users/register.php
+++ _ns/plugins/users/register.php
@@ -82,11 +82,23 @@ else {
       // User level (1 by default)
       $fields[] = 'level';
       $values[] = '1';
-      
+
+	  // Unique token to be sent to user on e-mail
+	  $emailtoken = hash('sha512', uniqid(openssl_random_pseudo_bytes(16), TRUE));
+      $fields[] = 'emailtoken';
+      $values[] = mysql_string($emailtoken);
+
       $query = 'INSERT INTO '.$table.' ('.implode(', ', $fields).') VALUES ('.implode(', ', $values).')';
       $conn->query($query);
 
-      header('http://'.NS_DOMAIN.'/n/welcome/');
+	  // Send user welcome e-mail with e-mail verification token
+
+		$email_sender = 'post@nettserier.no';
+	  $email_text = str_replace(array('{username}', '{pagetitle}', '{url}'), array($_POST['reg_username'], PAGE_TITLE, NS_DOMAIN.'/n/email-verify/'.$emailtoken), __('Dear {username},'."\r\n\r\n".'Welcome to {pagetitle}! We hope you will enjoy our wide selection of user-generated comics - or perhaps even add one (or more!) of your own?'."\r\n\r\n".'Your registration is almost complete. To finish it, we just need you to click this link to confirm that this is actually your e-mail address:'."\r\n".'{url}'."\r\n\r\n".'Have a wonderful day!'."\r\n\r\n".'Best wishes,'."\r\n".'{pagetitle}'))."\n";
+
+	  mail($_POST['reg_email'], str_replace('{pagetitle}', PAGE_TITLE, __('{pagetitle} Registration Confirmation')), $email_text, 'From: '.PAGE_TITLE.' '.$email_sender);
+
+      header('Location: '.NS_DOMAIN.'/n/welcome/');
       exit;
     
     }
