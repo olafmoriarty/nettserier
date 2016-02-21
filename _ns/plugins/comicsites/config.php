9deb69c50a28686ccf31510a277a8bf80e15d55f
Olaf Moriarty Solstrand <olafmoriarty@gmail.com>
2016-02-21 23:11:40 +0100

    Added owns_comics() function and dashboard link to "My comics" if comics exist

--- _ns/plugins/comicsites/config.php
+++ _ns/plugins/comicsites/config.php
@@ -5,4 +5,21 @@
 	// Setup 
 	$d_urls->add_line(['url' => 'new-comic', 'script' => $tpf.'newpage.php']);
 
-	$d_menu->add_line(['text' => __('Create new comic'), 'link' => '/n/dashboard/new-comic/']);
+	$d_menu->add_line(['text' => __('Create new comic'), 'link' => '/n/dashboard/new-comic/', 'order' => -10]);
+
+	if ($logged_in) {
+		if (owns_comics($user_info['id'])) {
+			$d_menu->add_line(['text' => __('My comics'), 'link' => '/n/dashboard/my-comics/', 'order' => 0]);
+		}
+	}
+
+	function owns_comics($id) {
+		global $conn;
+		$query = 'SELECT id FROM ns_user_comic_rel WHERE user = '.$id.' AND reltype IN (\'c\', \'e\')';
+		$result = $conn->query($query);
+		$num = $result->num_rows;
+		if ($num) {
+			return true;
+		}
+		return false;
+	}
