fbdbcd545e0bba9a998031b1f511676db175122e
Olaf Moriarty Solstrand <olafmoriarty@gmail.com>
2016-03-10 22:36:20 +0100

    More tweaking of file upload form

--- _ns/plugins/comicstrips/add.php
+++ _ns/plugins/comicstrips/add.php
@@ -1,15 +1,32 @@
 <?php
 
-$page = strtok('/');
+	$submitted = false;
+	$errors = false;
+	$error_array = array();
+	$comicname = comic_name($active_comic);
 
-if (!$page || $page == 1) {
+	if (isset($_POST) && isset($_POST['upload']) && $_POST['upload']) {
+		$submitted = true;
+	
+		// Let's assume there are uploaded images: How many are there?
+		$imgnum = count($_FILES['file']['name']);
+
+		for ($i = 0; $i < $imgnum; $i++) {
+			echo $_FILES['file']['name'][$i].'<br>';		
+		}
+		exit;
+
+	}
+
+	if (!$submitted || $errors) {
+
   $ns_title = __('Upload new comic strip or page');
-  $c .= '<h2>'.__('Upload new comic strip or page').'</h2>'."\n";
+  $c .= '<h2>'.str_replace('{comic}', htmlspecialchars($comicname), __('Upload new comic strip or page for "{comic}"')).'</h2>'."\n";
   $c .= '<h3>'.__('Step one: Upload files').'</h3>'."\n";
   $c .= '<p>'.__('You can upload a single comic strip or page, or multiple at the same time.').'</p>';
-  $c .= '<form method="post" action="/n/dashboard/my-comics/'.$active_comic.'/add/" enctype="multipart/form-data">';
-  
-  $c .= '<p><input type="file" name="file" id="file"></p>';
-  $c .= '<p><input type="submit" value="'.__('Upload and proceed').'"></p>';
+  $c .= '<form method="post" action="/n/dashboard/my-comics/'.$active_comic.'/add-strip/" enctype="multipart/form-data">';
+  $c .= '<p><input type="file" name="file[]" id="file" multiple></p>';
+  $c .= '<p><input type="submit" name="upload" value="'.__('Upload and proceed').'"></p>';
   $c .= '</form>';
-}
+
+	}
