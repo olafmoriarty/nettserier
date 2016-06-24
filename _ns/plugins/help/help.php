63e53bf76056aedf058cf8249dfec40b0a0cde1c
Olaf Moriarty Solstrand <olafmoriarty@gmail.com>
2016-06-24 21:14:57 +0200

    Patreon sponsors part 2: Why wasn't this file in the commit ...

--- _ns/plugins/help/help.php
+++ _ns/plugins/help/help.php
@@ -4,10 +4,11 @@ $folder = strtok('/');
 
 if ($folder == 'about') {
 	$c .= '<h2>'._('About us').'</h2>';
+	$c .= '<div class="about-us">';
 	$c .= '<p>'.str_replace(['{page}', '{company}'], [PAGE_TITLE, '<a href="'.'http://comicopia.no'.'" target="_blank">'.'Comicopia AS'.'</a>'], _('{page} is a Norwegian webcomics portal run and owned by {company}.')).'</p>';
 
 	$info['email'] = 'post@nettserier.no';
-	$info['snailmail'] = 'Comicopia AS<br>Postboks 40<br>1401 Ski<br>Norway';
+	$info['snailmail'] = 'Comicopia AS, Postboks 40, 1401 Ski, Norway';
 
 	$c .= '<h3 class="expand">'._('Contact us').'</h3>';
 	$c .= '<h4>'._('E-mail:').'</h4><p><a href="mailto:'.$info['email'].'">'.$info['email'].'</a></p>';
@@ -15,7 +16,7 @@ if ($folder == 'about') {
 
 	$c .= '<h3 class="expand">'._('Who are we?').'</h3>';
 	$c .= '<h4>'._('Managing director:').'</h4>'.'<p><a href="mailto:'.'olaf.moriarty.solstrand@comicopia.no'.'">'.'Olaf Moriarty Solstrand'.'</a></p>';
-	$c .= '<h4>'._('Board members:').'</h4>'.'<p>'.implode('<br>', ['Arne Bye', 'Rigmor Hanken', 'Bård Lilleøien', 'Olaf Moriarty Solstrand', 'Eirik Andreas Vik']).'</p>';
+	$c .= '<h4>'._('Board members:').'</h4>'.'<ul><li>'.implode('</li><li>', ['Arne Bye', 'Rigmor Hanken', 'Bård Lilleøien', 'Olaf Moriarty Solstrand', 'Eirik Andreas Vik']).'</li></ul>';
 
 	$c .= '<h3 class="expand">'.str_replace('{page}', PAGE_TITLE, _('Open source code used for {page}')).'</h3>';
 
@@ -31,6 +32,11 @@ if ($folder == 'about') {
 			$c .= '<p>'.str_replace('{name}', htmlspecialchars($os_arr[$i]['developer']), _('Developed by {name}')).'</p>';
 		}
 	}
+
+	// Having this hard-coded is STUPID. Must create a database over this. But hey, no rush, other things are more urgent ...
+	$c .= '<h3 class="expand">'._('Our sponsors').'</h3>'."\n".'<p>'.str_replace(['{page}', '{service}'], [PAGE_TITLE, '<a href="http://patreon.com/nettserier">Patreon.com</a>'], _('This is a list of the amazing people who are currently supporting {page} on {service} with $10/month or more. They\'re really incredible, and it would have been a lot more difficult to run {page} without them. THANK YOU!')).'</p>'."\n".'<ul><li>'.implode('</li><li>', ['Nils Petter Smeby', 'Eirik Andreas Vik', '<a href="http://dadaph.no" target="_blank" rel="nofollow">Bård Lilleøien</a>']).'</li></ul>';
+
+	$c .= '</div>';
 }
 elseif ($folder == 'cookies') {
 	$c .= '<h2>'._('Cookies').'</h2>';
