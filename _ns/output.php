764cabff636461d4ac821e9f47c889f942e9360f
Olaf Moriarty Solstrand <olafmoriarty@gmail.com>
2016-07-01 20:19:14 +0200

    Apparently charset must come before title tag ...

--- _ns/output.php
+++ _ns/output.php
@@ -1,14 +1,14 @@
 <!DOCTYPE html>
 <html lang="no">
 	<head>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width; initial-scale=1.0">
 		<title><?php
 		if ($ns_title) {
 			echo $ns_title;
 			echo $ns_tsep;
 		}
 		echo _(PAGE_TITLE); ?></title>
-		<meta charset="utf-8" />
-		<meta name="viewport" content="width=device-width; initial-scale=1.0">
 <?php
 	echo '<link rel="stylesheet" href="/_ns/styles/'.$ns_style.'/style.css">'."\n";
 	echo $head->return_text()."\n";
