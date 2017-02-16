d9e0583e9cda8acaefa80fd113d3944adbbf9023
olafmoriarty <olafmoriarty@gmail.com>
2017-02-16 17:38:50 +0100

    Restricted admin access, added link to moderation page

--- _ns/plugins/admin/admin.php
+++ _ns/plugins/admin/admin.php
@@ -6,22 +6,48 @@ $ns_title = _('Administration dashboard');
 // Path to this folder
 $tpf = NS_PATH.'plugins/'.basename(dirname(__FILE__)).'/';
 
+if ($logged_in && is_admin($user_info['id'])) {
 
-$folder = strtok('/');
+	$folder = strtok('/');
 
-if ($folder == 'plugins') {
-	include($tpf.'plugin_manager.php');
+	if ($folder == 'plugins') {
+		include($tpf.'plugin_manager.php');
+	}
+	elseif ($folder == 'languages') {
+		include($tpf.'language_manager.php');
+	}
+	
+// I NEED TO ADD AN ARRAYHANDLER FOR THIS, but for now ...
+
+	elseif ($folder == 'users') {
+		include('../moderation/users.php');
+	}
+
+	elseif ($folder == 'comments') {
+		include('../moderation/comments.php');
+	}
+
+	elseif (!$folder) {
+		$c .= '<h2>'._('Administration dashboard').'</h2>';
+
+// I NEED TO ADD AN ARRAYHANDLER FOR THIS, but for now ...
+		
+		$c .= '<h3>'._('Moderation').'</h3>';
+		$c .= '<ul>';
+		$c .= '<li><a href="/n/admin/users/">'._('Users').'</a></li>';
+		$c .= '<li><a href="/n/admin/comments/">'._('Comments').'</a></li>';
+		$c .= '</ul>';
+		
+		$c .= '<h3>'._('Advanced configuration').'</h3>';
+		$c .= '<ul>';
+		$c .= '<li><a href="/n/admin/plugins/">'._('Plugins').'</a></li>';
+		$c .= '<li><a href="/n/admin/languages/">'._('Languages').'</a></li>';
+		$c .= '</ul>';
+	}
+	else {
+		// *** 404!
+	}
 }
-elseif ($folder == 'languages') {
-	include($tpf.'language_manager.php');
+else {
+	// *** Show an error message here! The user is not supposed to be here!
 }
-elseif (!$folder) {
-	$c .= '<h2>'._('Administration dashboard').'</h2>';
-
-	$c .= '<ul>';
-	$c .= '<li><a href="/n/admin/plugins/">'._('Plugins').'</a></li>';
-	$c .= '<li><a href="/n/admin/languages/">'._('Languages').'</a></li>';
-	$c .= '</ul>';
-}
-
-?>
