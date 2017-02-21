4f6ea271a09a0b776186ac93768d4f3abd60996f
Olaf Moriarty Solstrand <olafmoriarty@gmail.com>
2017-02-21 22:13:18 +0100

    Max comments per day script fixed

--- _ns/plugins/moderation/config.php
+++ _ns/plugins/moderation/config.php
@@ -18,14 +18,14 @@
 	function too_many_comments() {
 		global $conn, $logged_in, $user_info;
 		
-		if ($logged_in && isset($user_info['id']) && user_info['level'] < 10) {
+		if ($logged_in && isset($user_info['id']) && $user_info['level'] < 10) {
 			// How many comments have the user written in the past day?
 			$query = 'SELECT COUNT(id) AS num FROM ns_comments WHERE regtime >= DATE_ADD(CURDATE(), INTERVAL -1 DAY)';
 			$result = $conn->query($query);
 			$r_arr = $result->fetch_assoc();
 			$num = $r_arr['num'];
 			$limit = 10;
-			if ($num > $limit) {
+			if ($num >= $limit) {
 				return str_replace('{n}', $limit, _('You have reached your daily comment quota. New and unverified users are limited to posting {n} comments per day. Sorry for the inconvenience, but this limitation will be lifted as soon as a moderator has looked over a few of your comments to verify that you\'re not a spambot.'));
 			}
 		}
